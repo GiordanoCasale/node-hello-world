d9cce02480c18add688afefd6c3e6ce4fd9f5642
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-15 15:25:04 +0200

    [DONE] exercise completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-console.log("hello Boolean")
+console.log("hello world")
+
+//ho inizializzato l'app con node
+//nel file index.js ho creato il console log di hello world
+//all'interno del json ho inserito start nello script per far apparire hello world direttamente dall'app
+//ho aggiunto watch allo script per abilitare la live reload del server
+//e infine ho usato il comando npm run watch per verificare che tutto funzionasse
